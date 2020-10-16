cdc33d6684548df9d84f4581cba811f029aced47
LiesseH <lhamidi.pro@gmail.com>
2020-10-16 11:04:54 +0200

    Ajout du timer pour le deplacement du heros

--- ProjetSpaceInvaders/Heros.cs
+++ ProjetSpaceInvaders/Heros.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace ProjetSpaceInvaders
 {
@@ -14,8 +15,33 @@ namespace ProjetSpaceInvaders
             m_iVie = 3;
             x = 20;
             y = 0;
+            Timer timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 250;
+            timer.Start();
             //Console.WriteLine("Je suis le Heros et j'ai " + m_iVie + " point de vie ");
             
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ConsoleKey touche = Console.ReadKey().Key;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(" ");
+
+            switch (touche)
+            {
+                //case ConsoleKey.Spacebar:
+                //break;
+                case ConsoleKey.LeftArrow:
+                    x = x - 10;
+                    break;
+                case ConsoleKey.RightArrow:
+                    x = x + 10;
+                    break;
+            }
+
+            ApparaitreHeros();
         }
 
         public void ApparaitreHeros()
@@ -27,27 +53,7 @@ namespace ProjetSpaceInvaders
         public override void ActionElement()
         {
 
-            do
-            {
-                ConsoleKey touche = Console.ReadKey().Key;
-                Console.SetCursorPosition(x, y);
-                Console.WriteLine(" ");
-
-                switch (touche)
-                {
-                    //case ConsoleKey.Spacebar:
-                    //break;
-                    case ConsoleKey.LeftArrow:
-                        x = x - 10;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        x = x + 10;
-                        break;
-                }
-                
-                ApparaitreHeros();
-
-            } while (true);
+            
                 
         }
 
--- ProjetSpaceInvaders/Program.cs
+++ ProjetSpaceInvaders/Program.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProjetSpaceInvaders
 {
     class Program
     {
-       
+        static bool GameOver = false;
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
             ElementsEspace NewGame = new ElementsEspace();
-            
-           
+
+            while (!GameOver)
+            {
+                Thread.Sleep(250);
+            }
 
 
             Console.ReadLine();
